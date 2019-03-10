f77dc2117443236060e005135c0f90ebc82cb0bb
alireza aghaei <appleiran66@gmail.com>
2019-03-10 10:46:48 +0330

    first commit

--- config/database.php
+++ config/database.php
@@ -42,17 +42,19 @@ return [
 
         'mysql' => [
             'driver' => 'mysql',
-            'host' => env('DB_HOST', '127.0.0.1'),
-            'port' => env('DB_PORT', '3306'),
-            'database' => env('DB_DATABASE', 'forge'),
-            'username' => env('DB_USERNAME', 'forge'),
-            'password' => env('DB_PASSWORD', ''),
+            'host' => 'localhost',
+            'port' => '3306',
+            'database' => 'farsar',
+            'username' => 'root',
+            'password' => 'root',
             'unix_socket' => env('DB_SOCKET', ''),
             'charset' => 'utf8mb4',
             'collation' => 'utf8mb4_unicode_ci',
             'prefix' => '',
             'prefix_indexes' => true,
             'strict' => true,
+            'unix_socket'   => '/Applications/MAMP/tmp/mysql/mysql.sock',
+
             'engine' => null,
         ],
 
